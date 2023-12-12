a7f5b987bf1cfd06490d89f85da054329fc0d6e1
jkrrrr <102986583+jkrrrr@users.noreply.github.com>
2023-12-12 16:30:39 +0000

    feat: password hashing stored to file

--- src/main/java/org/SFM/CryptoHandler.java
+++ src/main/java/org/SFM/CryptoHandler.java
@@ -14,6 +14,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 
 public class CryptoHandler {
@@ -80,7 +81,7 @@ public class CryptoHandler {
 
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-        return(hash.toString());
+        return(new String(hash, StandardCharsets.UTF_8));
     }
 
 
