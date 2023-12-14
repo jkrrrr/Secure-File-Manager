b0c847c19a86c923a1a42d83f868b42b36a7c842
jkrrrr <102986583+jkrrrr@users.noreply.github.com>
2023-12-14 11:04:38 +0000

    docs: javadocs

--- src/main/java/org/SFM/PasswordHandler.java
+++ src/main/java/org/SFM/PasswordHandler.java
@@ -13,6 +13,13 @@ public class PasswordHandler {
     private final CryptoHandler cryptoHandler;
 
 
+    /**
+     * Password management
+     * @param path path to file containing hashed passwords
+     * @throws IOException
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     */
     private PasswordHandler(String path) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException {
         this.path = path;
         this.hashes = new ArrayList<>();
@@ -21,19 +28,36 @@ public class PasswordHandler {
         updateHashes();
     }
 
-
+    /**
+     * Singleton checking
+     * @param path path to file contained hashed passwords
+     * @return instance of PasswordHandler
+     * @throws Exception
+     */
     public static PasswordHandler getInstance(String path) throws Exception{
         if (instance == null)
             instance = new PasswordHandler(path);
         return instance;
     }
 
+    /**
+     * Checks to see if the given password is contained within the password file
+     * @param password password to check
+     * @return true if in password file, otherwise false
+     * @throws NoSuchAlgorithmException
+     */
     public boolean checkPassword(String password) throws NoSuchAlgorithmException {
         // Hash password, and check if it is in the list
         String hash = this.cryptoHandler.processPassword(password);
         return this.hashes.contains(hash);
     }
 
+    /**
+     * Appends a password to the file, hashed
+     * @param password password to hash and store
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     */
     public void insertPassword(String password) throws NoSuchAlgorithmException, IOException {
         System.out.println("Adding " + password);
         String hash = this.cryptoHandler.processPassword(password);
@@ -49,11 +73,10 @@ public class PasswordHandler {
         updateHashes();
     }
 
-    public void print(){
-        System.out.println(this.hashes.size());
-        System.out.println(this.hashes.toString());
-    }
-
+    /**
+     * Refreshes the class' list of passwords based on the ones in the file
+     * @throws IOException
+     */
     private void updateHashes() throws IOException {
         this.hashes = new ArrayList<>();
 
