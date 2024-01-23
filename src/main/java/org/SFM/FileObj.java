9ee10e79088e17cf6e2dab0884ef9cb0b49e5599
jkrrrr <102986583+jkrrrr@users.noreply.github.com>
2024-01-23 12:27:02 +0000

    fix: file object class

---
+++ src/main/java/org/SFM/FileObj.java
@@ -0,0 +1,20 @@
+package org.SFM;
+
+public class FileObj {
+    private String path;
+
+    public FileObj(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+
+}
+
